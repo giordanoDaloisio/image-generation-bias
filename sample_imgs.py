4cf87d1de880f9f8a7c2fb3b795fbef5341ee573
giordanoDaloisio <giordano.daloisio@graduate.univaq.it>
2025-03-24 10:37:41 +0100

    add cog img inspection

--- sample_imgs.py
+++ sample_imgs.py
@@ -9,26 +9,28 @@ name_list = []
 
 
 
-base_path = os.path.join('manual_inspection', 'se')
-rmtree(base_path)
+base_path = os.path.join('manual_inspection', 'cog', 'general')
+# rmtree(base_path)
 os.makedirs(base_path, exist_ok=True)
 
 
-for folder in os.listdir('imgs'):
-    if 'portrait_of_a_Software_Engineer' in folder:
-        for file in os.listdir(os.path.join('imgs', folder)):
-            print(file)
-            name_list.append(file.replace(" ","_").replace("\n",""))
+for folder in os.listdir('imgs_cog/general'):
+    if folder == '.DS_Store':
+        continue
+    for file in os.listdir(os.path.join('imgs_cog/general', folder)):
+        print(file)
+        name_list.append(file.replace(" ","_").replace("\n",""))
 
 manual_imgs = sample(name_list, 89)
 
-for folder in os.listdir('imgs'):
-    if 'portrait_of_a_Software_Engineer' in folder:
-        for file in os.listdir(os.path.join('imgs', folder)):
-            if file.replace(" ","_").replace("\n","") in manual_imgs:
-                print(file)
-                copy(os.path.join('imgs', folder, file), os.path.join('manual_inspection', 'se', file))
+for folder in os.listdir('imgs_cog/general'):
+    if folder == '.DS_Store':
+        continue
+    for file in os.listdir(os.path.join('imgs_cog/general', folder)):
+        if file.replace(" ","_").replace("\n","") in manual_imgs:
+            print(file)
+            copy(os.path.join('imgs_cog/general', folder, file), os.path.join(base_path, file))
 
 pd.DataFrame({
     'prompt': manual_imgs,
-}).to_csv(os.path.join('manual_inspection', 'se', 'img_list.csv'))
+}).to_csv(os.path.join(base_path, 'img_list.csv'))
