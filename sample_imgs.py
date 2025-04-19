8448451463c9f27bf12bce4e1c0c9e7aa9d24334
giordanoDaloisio <giordano.daloisio@graduate.univaq.it>
2025-04-19 17:24:12 +0200

    manual labels

--- sample_imgs.py
+++ sample_imgs.py
@@ -3,33 +3,39 @@ from random import sample
 from shutil import copy, rmtree
 import pandas as pd
 import numpy as np
+from argparse import ArgumentParser
 
+parser = ArgumentParser()
+parser.add_argument('--model')
+
+args = parser.parse_args()
 
 name_list = []
 
 
 
-base_path = os.path.join('manual_inspection', 'imgs_3_fair', 'general')
+base_path = os.path.join('manual_inspection', args.model)
 # rmtree(base_path)
 os.makedirs(base_path, exist_ok=True)
 
+img_path = os.path.join(args.model, 'SE')
 
-for folder in os.listdir('imgs_cog/general'):
+for folder in os.listdir(img_path):
     if folder == '.DS_Store':
         continue
-    for file in os.listdir(os.path.join('imgs_cog/general', folder)):
+    for file in os.listdir(os.path.join(img_path, folder)):
         print(file)
         name_list.append(file.replace(" ","_").replace("\n",""))
 
 manual_imgs = sample(name_list, 89)
 
-for folder in os.listdir('imgs_cog/general'):
+for folder in os.listdir(img_path):
     if folder == '.DS_Store':
         continue
-    for file in os.listdir(os.path.join('imgs_cog/general', folder)):
+    for file in os.listdir(os.path.join(img_path, folder)):
         if file.replace(" ","_").replace("\n","") in manual_imgs:
             print(file)
-            copy(os.path.join('imgs_cog/general', folder, file), os.path.join(base_path, file))
+            copy(os.path.join(img_path, folder, file), os.path.join(base_path, file))
 
 pd.DataFrame({
     'prompt': manual_imgs,
